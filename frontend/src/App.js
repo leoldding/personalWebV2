305c8df4c1bfb0beb64754066c5c7c14a4c33a4d
leoldding <leo.ld.ding@gmail.com>
2023-01-20 16:01:47 -0500

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components/css/styles.css'
+class App extends React.Component {
+
+    render() {
+        return(
+            <h1>Leo Ding</h1>
+        )
+    }
+}
+
+export default App;
